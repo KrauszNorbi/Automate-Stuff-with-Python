f01acf0700f6b7b3fe880c9aa93d421cd77e014c
unknown <blamro@gmail.com>
2020-04-13 16:28:29 +0200

    Unit Testing

--- city_functions.py
+++ city_functions.py
@@ -6,3 +6,23 @@ def cityFunction(city_name, country_name, population = ''):
 
     return getCityCountry
 
+print(cityFunction('kosice', 'slovakia', 240000))
+print(cityFunction('roskilde', 'denmark'))
+
+class Employee():
+    def __init__(self, first_name, last_name, annual_salary):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.annual_salary = annual_salary
+
+    def give_raise(self, raise_amount = 5000):
+        self.raise_amount = raise_amount
+        if raise_amount:
+            raised_salary = int(self.annual_salary) + self.raise_amount
+        else:
+            raised_salary = int(self.annual_salary) + raise_amount
+        return raised_salary
+
+norbi = Employee('Norbi', 'K', 2000)
+print(norbi.give_raise())
+print(norbi.give_raise(1000))
--- test_cities.py
+++ test_cities.py
@@ -1,13 +1,29 @@
 import unittest
-from city_functions import cityFunction
+from city_functions import cityFunction, Employee
+
 
 class CityCountryTests(unittest.TestCase):
+    '''Tests for methods from city_functions.py'''
+    def setUp(self):
+        '''Create an object of Employee class '''
+        self.norbi = Employee('Norbi', 'K', 2000)
+
     def test_city_country(self):
+        '''Test cityFunction method with 2 parameters'''
         city_function = cityFunction('kosice', 'slovakia')
         self.assertEqual(city_function, 'Kosice, Slovakia')
 
     def test_city_country_population(self):
+        '''Test cityFunction method with 3 parameters'''
         city_function_pop = cityFunction('kosice', 'slovakia', 240000)
         self.assertEqual(city_function_pop, 'Kosice, Slovakia, 240000')
+
+    def test_give_default_raise(self):
+        '''Test give_raise method with default value'''
+        self.assertEqual(self.norbi.give_raise(), 7000)
+
+    def test_give_custom_raise(self):
+        '''Test give_raise method with custom value'''
+        self.assertEqual(self.norbi.give_raise(1000), 3000)
 
 unittest.main()
